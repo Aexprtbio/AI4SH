6e00248cc128b854b606cea32130bd7e1949b274
Alexi Prétat <alexis.pretat@gmail.com>
2025-08-22 15:31:42 +0200

    Add GLM modeling and prediction workflow for soil data
    Introduces a GLM (Poisson, sqrt link) to model cum_taxa as a function of lapsed_time and observer, adds exploratory violin plot, model diagnostics, ANOVA, R-squared calculation, and prediction visualization using ggplot2. Also loads additional packages and sets a random seed for reproducibility.

--- safari_model.R
+++ safari_model.R
@@ -3,13 +3,18 @@
 
 setwd('D:/GitHub/AI4SH')
 
-soil <- read.csv('soil_for_mod.csv', h=TRUE)
+soil <- read.csv('soil_for_mod.csv', h=TRUE, stringsAsFactor=TRUE)
 summary(soil)
 
 
 
 # PACKAGES
 
+library(car)
+library(ggplot2)
+library(MASS)
+library(MuMIn)
+library(RVAideMemoire)
 library(vegan)
 library(glmmTMB)
 
@@ -19,10 +24,52 @@ library(glmmTMB)
 
 class(soil$cum_taxa)
 
+# GRAPHIC DATA
+
+p <- ggplot(soil, aes(x=observer, y=lapsed_time))
+p+geom_violin()
+
 #######################################################################
 #######################################################################
 # MODELISATION
 
-formula <- (cum_taxa ~ lapsed_time + month + transect_id)
+set.seed(7)
 
-mod1 <- glmmTMB(formula, data=soil)
+formula <- ((cum_taxa) ~ (lapsed_time*observer))
+
+mod1 <- glm(formula, data=soil, family=poisson(link="sqrt"))
+
+
+#---------------------------------------------------------------------#
+summary(mod1) 										
+
+# AIC: 2031.5
+
+
+# Graphics -----------------------------------------------------------#
+plotresid(mod1)
+
+
+
+
+######################################################################
+# Analysis
+
+Anova(mod1)
+
+r.squaredGLMM(mod1)
+
+
+# predictions --------------------------------------------------------#
+
+newdata <- as.data.frame(matrix(ncol=2, nrow=1000))
+colnames(newdata) <- c('lapsed_time', 'observer')
+
+newdata$lapsed_time <- floor(runif(1000, min=0, max=3600))
+newdata$observer <- rep(c('Alex','Jerome', 'Collector1', 'Collector2', 'Collector3'), times=200)
+
+
+newdata$predictions<-predict(mod1, newdata=newdata, type='response')
+
+p <- ggplot(newdata, aes(x = lapsed_time, y = predictions, color=observer))
+p + geom_line(linewidth = 1.1)
